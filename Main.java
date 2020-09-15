177f585bbdf68168c0889495012a7a2b068dac59
Gormi3815 <gormi3815@wrdsb.ca>
2020-09-15 13:48:35 +0000

    made the program tell the user what to do while playing FizzBuzz

--- Main.java
+++ Main.java
@@ -1,15 +1,34 @@
-/**
+import java.util.Scanner;
+
+/** This program will tell you what to say durring FizzBuzz
  *
- * @author 
+ * @author Isaac
  */
 public class Main {
 
   /**
    * The method that is executed when you hit the run button.
+   * 
    * @param args the command line arguments
    */
   public static void main(String[] args) {
     // TODO code application logic here
-    
+    Scanner input = new Scanner(System.in);
+    System.out.println("Please enter a number to play FizzBuzz.");
+    int number = input.nextInt();
+    double number_Fizz = number % 3;
+    double number_Buzz = number % 5;
+    if (number_Buzz == 0 && number_Fizz == 0) {
+      System.out.println("You should say FizzBuzz");
+    } else if (number_Fizz == 0) {
+      System.out.println("You should say Fizz.");
+    } else if (number_Buzz == 0) {
+      System.out.println("You should say Buzz.");
+    } else if (number_Fizz ==1 ) {
+      System.out.println("You should say " + number + ".");
+
+    }
+
   }
+
 }
